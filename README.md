7d2d390bb0ed68de9e8b818196c5f7173df4b049
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-11-02 11:41:58 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+#  Mini Todo Management Project 
+- Spring Boot
+- Spring MVC 
+- Spring Security 
+- JSP 
+- Hibernate 
+- MySQL
+
---
+++ src/main/com/wtricks/todomanagement/TodoManagement.java
@@ -0,0 +1,12 @@
+package net.guides.springboot.todomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagement {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagement.class, args);
+	}
+}
